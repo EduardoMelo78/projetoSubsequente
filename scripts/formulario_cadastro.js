24d00f54e8f44d3552d8bdf3b3bdd8e94f5201c5
Eduardo Melo <meloedu105@gmail.com>
2025-01-18 11:54:16 -0300

    Cadastro pronto e lindo

--- scripts/formulario_cadastro.js
+++ scripts/formulario_cadastro.js
@@ -1,14 +1,16 @@
 function showForm2() {
     document.getElementById('pessoal').style.display = 'none';
-    document.getElementById('endereco').style.display = 'block';
+    document.getElementById('endereco').style.display = 'flex';
 }
 
 function showForm1() {
     document.getElementById('endereco').style.display = 'none';
-    document.getElementById('pessoal').style.display = 'block';
+    document.getElementById('pessoal').style.display = 'flex';
     
 }
 
 function cadastrar(){
-    
+
+    /*Após cadastrar o usuario ir para a página de login para autenticar*/
+    window.location.href = "login.html";
 }
