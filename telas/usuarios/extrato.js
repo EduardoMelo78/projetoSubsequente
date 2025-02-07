2330323df69961ce750ab3a4ffc346721fb5cfc3
Eduardo Melo <meloedu105@gmail.com>
2025-02-07 15:48:04 -0300

    Post falta ajuste do double

--- telas/usuarios/extrato.js
+++ telas/usuarios/extrato.js
@@ -1,4 +1,9 @@
-fetch('http://localhost:8080/pagamentos')
+obterdados();
+
+
+function obterdados(){
+
+    fetch('http://localhost:8080/pagamentos')
   .then(resposta => resposta.json())
   .then(pagamentos => {
     const tabelaHtml = document.querySelector("#tabela")
@@ -16,4 +21,34 @@ fetch('http://localhost:8080/pagamentos')
         
     })
   })
+  .catch(err => console.log(err))
+
+}
+
+document.querySelector("#addPagamento").addEventListener("submit", salvar)
+
+function salvar(event){
+
+    event.preventDefault()
+
+    const formulario = new FormData(event.target)
+
+    pagamento = {
+        "tipoPagamento": formulario.get("tipoPagamento"),
+        "valorReais": formulario.get("valorReais"),
+        "valorPontos": formulario.get("valorPontos")
+    }
+
+        fetch('http://localhost:8080/pagamentos',{
+            method : 'POST',
+            headers : {'Content-Type' : 'application/json'},
+            body : JSON.stringify(pagamento)})
+            .then( resposta => {
+            console.log('resposta do Servidor:', resposta);
+            if(resposta.ok)
+                obterdados()
+})
+.catch(err => console.log(err))
+}
+
 
